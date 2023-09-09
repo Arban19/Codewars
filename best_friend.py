b424122b70fe562084e32909d57d82106be933b5
Anirban Kar <anirban.k59@gmail.com>
2023-09-09 22:05:58 +0530

    bug fixes best_friend

--- best_friend.py
+++ best_friend.py
@@ -5,10 +5,8 @@ def best_friend(text, a, b):
             char = text[i]
             char_next = text[i + 1]
         except IndexError:
-            char_next == " "
+            char_next = " "
         if char == a and char_next != b:
                 return False
         i += 1
     return True
-print(best_friend("rjw kdaosm kh", "h", "u"))
-print(best_friend("xmrhzpyo thzidhz ahzzh", "h", "z"))
