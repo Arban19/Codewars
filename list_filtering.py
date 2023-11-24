9b5e4f5d84e27659ae7adf594b9f8607015d6d97
Anirban Kar <anirban.k59@gmail.com>
2023-11-24 15:26:19 +0530

    list filtering alternative

--- list_filtering.py
+++ list_filtering.py
@@ -12,3 +12,10 @@ def filter_list(l):
             if str(item).isdigit():
                 result.append(item)
     return result
+
+def filter_list(l):
+    new_list =[]
+    for x in l:
+        if type(x) != str:
+            new_list.append(x)
+    return new_list
