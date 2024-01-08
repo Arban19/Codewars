be79a883c68d40c71d61dc679f67b24b3c63c513
Anirban Kar <anirban.k59@gmail.com>
2024-01-08 21:02:30 +0530

    add new codewar challenge

--- upperCase.py
+++ upperCase.py
@@ -1,27 +1,27 @@
 def make_upper_case(s):
 	print(s.upper())
 
-make_upper_case("Hello")
+make_upper_case("anshu")
 
 
-def who_is_paying(name):
-    if len(name) > 2:
-        return [name, name[0:2]]
-    else:
-        return [name]
+# def who_is_paying(name):
+#     if len(name) > 2:
+#         return [name, name[0:2]]
+#     else:
+#         return [name]
 
-print(who_is_paying("Zack"))
+# print(who_is_paying("Zack"))
 
-def cookie(x):
-    result = " "
-    if isinstance(x,str) == True:
-        result = "Who ate the last cookie? It was Zach!"
-    elif isinstance(x,float) == True and type(x) is not bool:
-        result = "Who ate the last cookie? It was Monica!"
-    elif isinstance(x,int) == True and type(x) is not bool:
-        result = "Who ate the last cookie? It was Monica!"
-    else:
-        result = "Who ate the last cookie? It was the dog!"
-    return result
+# def cookie(x):
+#     result = " "
+#     if isinstance(x,str) == True:
+#         result = "Who ate the last cookie? It was Zach!"
+#     elif isinstance(x,float) == True and type(x) is not bool:
+#         result = "Who ate the last cookie? It was Monica!"
+#     elif isinstance(x,int) == True and type(x) is not bool:
+#         result = "Who ate the last cookie? It was Monica!"
+#     else:
+#         result = "Who ate the last cookie? It was the dog!"
+#     return result
 
-cookie(None)
+# cookie(None)
