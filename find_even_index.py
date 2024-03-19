7b5729e3ea4fc2c36b4389b8632b17b8babfba7b
Anirban Kar <anirban.k59@gmail.com>
2024-03-19 20:46:15 +0530

    add new codewar challenge with instructions

--- find_even_index.py
+++ find_even_index.py
@@ -2,20 +2,10 @@ def find_even_index(arr):
     left, right = 0, 0
 
     for i in range(len(arr)):
-        if i == 0:
-            left = 0
-            right = sum(arr[1:])
-        elif i == (len(arr)-1):
-            left = sum(arr[:-1])
-            right = 0
-        else:
-            left = sum(arr[:i])
-            right = sum(arr[i+1:])
+        left = sum(arr[:i])
+        right = sum(arr[i+1:])
 
         if left == right:
             return i
-        else:
-            continue
-    return -1
 
-print(find_even_index([1,2,3,4,3,2,1]))
+    return -1
